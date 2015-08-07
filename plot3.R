1d8b03e785f4e72873d67ff3cbb105d8325c143b
Carlos Cunha <ceacunha@Carloss-MacBook-Pro.local>
2015-08-06 23:42:35 -0300

    Date filter
    changing code for better date conversion and filter

--- plot3.R
+++ plot3.R
@@ -4,11 +4,14 @@ library(lubridate)
 # reads data for source indicating the missing value mark '?' and separator ';'
 consumption <- read.table("household_power_consumption.txt",
                           na.strings = "?", sep = ";", header = TRUE,
-                          stringsAsFactors = FALSE)
+                          stringsAsFactors = FALSE,
+                          colClasses = c("character", "character", "numeric",
+                                         "numeric", "numeric", "numeric", "numeric",
+                                         "numeric", "numeric" ))
 
 # filter the data based on assingment indication for date
-consumption <- consumption[consumption$Date == "1/2/2007" |
-                             consumption$Date == "2/2/2007",]
+consumption <- consumption[as.Date(consumption$Date, format = "%d/%m/%Y") == "2007-02-01" |
+                             as.Date(consumption$Date, format = "%d/%m/%Y") == "2007-02-02",]
 
 # combines the Date and Time columns in a single timestamp column
 # for required graphics
